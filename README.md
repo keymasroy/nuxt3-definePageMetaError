899295cfdff2e9e7b0a25b979e4ea1c7a1493559
keymasroy <keymasroy@gmail.com>
2022-11-16 17:56:05 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nuxt3-definePageMetaError
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nuxt-3-j6fsqw)
---
+++ modules/index.ts
@@ -0,0 +1,10 @@
+import { defineNuxtModule } from '@nuxt/kit';
+
+export default defineNuxtModule({
+  meta: {
+    name: 'custom',
+  },
+  setup(option, nuxt) {
+    nuxt.hooks;
+  },
+});
